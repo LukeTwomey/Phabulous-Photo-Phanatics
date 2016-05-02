d958bb988ea74161a64e3da75b2f06fa56c8a508
Luke Twomey <luke.twomey85@gmail.com>
2016-05-02 22:01:21 +0100

    Add tile h2 styling.

--- index.php
+++ index.php
@@ -13,7 +13,11 @@
 
             <div class="page-content index">
                 <img src="/images/holding-camera-s.jpg" srcset="/images/holding-camera-m.jpg 640w, /images/holding-camera-l.jpg 1200w" alt="Man holding camera" />
-                <img src="/images/brick-wall-s.jpg" srcset="/images/brick-wall-m.jpg 640w, /images/brick-wall-l.jpg 1200w" alt="Model posing in front of brick wall"/>
+
+                <div class="modelling tile">
+                    <img src="/images/brick-wall-s.jpg" srcset="/images/brick-wall-m.jpg 640w, /images/brick-wall-l.jpg 1200w" alt="Model posing in front of brick wall"/>
+                    <h2>Modelling</h2>
+                </div>
             </div>
 
             <?php include("footer.php"); ?>
