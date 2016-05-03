c60c8232cbf3dfc65239ddfe571595554bb190ae
Luke Twomey <luke.twomey85@gmail.com>
2016-05-03 21:20:49 +0100

    Change modelling to fashion.

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
             <div class="page-content index">
                 <img src="/images/holding-camera-s.jpg" srcset="/images/holding-camera-m.jpg 640w, /images/holding-camera-l.jpg 1200w" alt="Man holding camera" />
 
-                <div class="modelling tile">
+                <div class="fashion tile">
                     <img src="/images/brick-wall-s.jpg" srcset="/images/brick-wall-m.jpg 640w, /images/brick-wall-l.jpg 1200w" alt="Model posing in front of brick wall"/>
-                    <h2>Modelling</h2>
+                    <h2>Fashion</h2>
                 </div>
 
                 <div class="weddings tile">
