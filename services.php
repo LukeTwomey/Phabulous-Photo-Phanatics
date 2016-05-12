80180fb9541801bcfeda6b1ca800a1571154d5b1
Luke Twomey <luke.twomey85@gmail.com>
2016-05-12 21:23:51 +0100

    Add services page title and styling for rest of page template div.

--- services.php
+++ services.php
@@ -12,7 +12,10 @@
 			<?php include("header.php"); ?>
 
             <div class="page-content services">
-
+                <img class="banner-image" src="/images/services-s.jpg" srcset="/images/services-m.jpg 640w, /images/services-l.jpg 1200w" alt="Close crop of a camera"/>
+                <div class="rest-of-page">
+                    <h1>Services</h1>
+                </div>
             </div>
 
             <?php include("footer.php"); ?>
