c2d899e469b7bca9f781035c18177475b87c855e
Luke Twomey <luke.twomey85@gmail.com>
2016-05-12 19:53:42 +0100

    Add hyperlink to call to action button.

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
                 <div class="text-block studio">
                     <h1><strong><span>Phabulous Photo Phanatics</span></strong><br/> are the premier photography service provider in the UK.</h1>
-                    <a href="#" class="button">Find Out More</a>
+                    <a href="about.php" class="button">Find Out More</a>
                 </div>
 
                 <div class="fashion tile">
