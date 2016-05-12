092a0ec41f9d1ec4868d5fea69249dcea726ee39
Luke Twomey <luke.twomey85@gmail.com>
2016-05-12 21:37:44 +0100

    Add styling for headers and paragraph text.

--- services.php
+++ services.php
@@ -15,6 +15,8 @@
                 <img class="banner-image" src="/images/services-s.jpg" srcset="/images/services-m.jpg 640w, /images/services-l.jpg 1200w" alt="Close crop of a camera"/>
                 <div class="rest-of-page">
                     <h1>Services</h1>
+                    <h2>Phabulous Photo Phanatics provide our customers with professional, quality photography services</h2>
+                    <p>Our expert photographers will consult with you closely, to ensure the work undertaken meets your exacting standards.</p>
                 </div>
             </div>
 
