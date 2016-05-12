6adf05d9e5eb446b9a3d06e386a3fb2dcf135d02
Luke Twomey <luke.twomey85@gmail.com>
2016-05-12 21:05:22 +0100

    Create blank services page.

---
+++ services.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<head>
+	<?php include("head.php"); ?>
+</head>
+
+<body>
+	<div id="fouc">
+		<?php include("nav.php"); ?>
+
+        <div class="site-wrap">
+			<?php include("header.php"); ?>
+
+            <div class="page-content services">
+
+            </div>
+
+            <?php include("footer.php"); ?>
+		</div>
+	</div>
+
+    <?php include("back-to-top.php"); ?>
+    <?php include("javascript-files.php"); ?>
+
+</body>
