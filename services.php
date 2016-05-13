49706f786fa6a217370c1bbb1126a3d917d2d6ee
Luke Twomey <luke.twomey85@gmail.com>
2016-05-13 21:24:12 +0100

    Add further wedding services details. Style the image hover state.

--- services.php
+++ services.php
@@ -16,10 +16,19 @@
                 <div class="rest-of-page">
                     <div class="text-block white">
                         <h1>Services</h1>
-                        <h2>Phabulous Photo Phanatics provide our customers with professional, quality photography services</h2>
+                        <h3>Phabulous Photo Phanatics provide our customers with professional, quality photography services</h3>
                         <p>Our expert photographers will consult with you closely, to ensure the work undertaken meets your exacting standards.</p>
                     </div>
-                    <img src="/images/services-wedding-s.jpg" srcset="/images/services-wedding-m.jpg 640w, /images/services-wedding-l.jpg 1200w" alt="Married Couple"/>
+                    <div class="image-block">
+                        <a href="weddings.php">
+                            <img src="/images/services-wedding-s.jpg" srcset="/images/services-wedding-m.jpg 640w, /images/services-wedding-l.jpg 1200w" alt="Married Couple"/>
+                        </a>
+                    </div>
+                    <div class="text-block white">
+                        <a href="weddings.php"><h2>Weddings</h2></a>
+                        <p>We know how important your special day is. Leave everything to us and relax, safe in the knolwedge that the experts have it all under control.</p>
+                        <a href="weddings.php" class="button">Find Out More</a>
+                    </div>
                 </div>
             </div>
 
