3ce7a22224ce0908da3bdea05a3cabf1927527a2
Luke Twomey <luke.twomey85@gmail.com>
2016-05-13 20:55:03 +0100

    Add wedding services photo and adjust styles for page formatting.

--- services.php
+++ services.php
@@ -14,9 +14,12 @@
             <div class="page-content services">
                 <img class="banner-image" src="/images/services-s.jpg" srcset="/images/services-m.jpg 640w, /images/services-l.jpg 1200w" alt="Close crop of a camera"/>
                 <div class="rest-of-page">
-                    <h1>Services</h1>
-                    <h2>Phabulous Photo Phanatics provide our customers with professional, quality photography services</h2>
-                    <p>Our expert photographers will consult with you closely, to ensure the work undertaken meets your exacting standards.</p>
+                    <div class="text-block white">
+                        <h1>Services</h1>
+                        <h2>Phabulous Photo Phanatics provide our customers with professional, quality photography services</h2>
+                        <p>Our expert photographers will consult with you closely, to ensure the work undertaken meets your exacting standards.</p>
+                    </div>
+                    <img src="/images/services-wedding-s.jpg" srcset="/images/services-wedding-m.jpg 640w, /images/services-wedding-l.jpg 1200w" alt="Married Couple"/>
                 </div>
             </div>
 
