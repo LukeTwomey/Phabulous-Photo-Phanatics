14ab546602b327b277f094ed5557717db69a0127
Luke Twomey <luke.twomey85@gmail.com>
2016-05-15 19:43:25 +0100

    Create blank "gallery" page.

---
+++ gallery.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<head>
+	<?php include("head.php"); ?>
+</head>
+
+<body>
+	<div id="fouc">
+		<?php include("nav.php"); ?>
+
+        <div class="site-wrap">
+			<?php include("header.php"); ?>
+
+            <div class="page-content gallery">
+                <img class="banner-image" src="/images/gallery-s.jpg" srcset="/images/gallery-m.jpg 640w, /images/gallery-l.jpg 1200w" alt="Man holding camera outside"/>
+                <div class="rest-of-page">
+
+
+                </div>
+            </div>
+
+            <?php include("footer.php"); ?>
+		</div>
+	</div>
+
+    <?php include("back-to-top.php"); ?>
+    <?php include("javascript-files.php"); ?>
+
+</body>
