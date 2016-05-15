7c5820d96f58844d03de9ed711258508233099c0
Luke Twomey <luke.twomey85@gmail.com>
2016-05-15 19:45:18 +0100

    Add gallery page intro text.

--- gallery.php
+++ gallery.php
@@ -14,8 +14,10 @@
             <div class="page-content gallery">
                 <img class="banner-image" src="/images/gallery-s.jpg" srcset="/images/gallery-m.jpg 640w, /images/gallery-l.jpg 1200w" alt="Man holding camera outside"/>
                 <div class="rest-of-page">
-
-
+                    <div class="text-block white">
+                        <h1>Gallery</h1>
+                        <p>A selection of our best work.</p>
+                    </div>
                 </div>
             </div>
 
