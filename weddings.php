fcad28698c17d5ea7d99a9d372df69dd2734336b
Luke Twomey <luke.twomey85@gmail.com>
2016-05-22 16:11:56 +0100

    Create weddings page.

---
+++ weddings.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+
+<head>
+	<?php include("head.php"); ?>
+</head>
+
+<body>
+	<div id="fouc">
+		<?php include("nav.php"); ?>
+
+        <div class="site-wrap">
+			<?php include("header.php"); ?>
+
+            <div class="page-content weddings">
+                <img class="banner-image" src="/images/weddings-s.jpg" srcset="/images/weddings-m.jpg 640w, /images/weddings-l.jpg 1200w" alt="Close crop of a camera"/>
+                <div class="rest-of-page">
+
+                    <div class="text-block white">
+                        <div class="content">
+                            <h1>Weddings</h1>
+                            <h3>With extensive experience in the fashion industry, we know what it takes to produce great shots</h3>
+                            <p>Whether you're a seasoned pro, or an aspiring young model, we can tailor our services to suit your requirements.</p>
+                        </div>
+                    </div>
+
+                    <div class="image-block four">
+                        <a class="fancybox" href="/images/b-and-w-l.jpg">
+                            <img src="/images/b-and-w-s.jpg" srcset="/images/b-and-w-m.jpg 640w, /images/b-and-w-l.jpg 1200w" alt="Fashion Model Posing"/>
+                        </a>
+                        <a class="fancybox" href="/images/b-and-w-2-l.jpg">
+                            <img src="/images/b-and-w-2-s.jpg" srcset="/images/b-and-w-2-m.jpg 640w, /images/b-and-w-2-l.jpg 1200w" alt="Fashion Model Posing"/>
+                        </a>
+                        <a class="fancybox" href="/images/b-and-w-3-l.jpg">
+                            <img src="/images/b-and-w-3-s.jpg" srcset="/images/b-and-w-3-m.jpg 640w, /images/b-and-w-3-l.jpg 1200w" alt="Fashion Model Posing"/>
+                        </a>
+                        <a class="fancybox" href="/images/b-and-w-4-l.jpg">
+                            <img src="/images/b-and-w-4-s.jpg" srcset="/images/b-and-w-4-m.jpg 640w, /images/b-and-w-4-l.jpg 1200w" alt="Fashion Model Posing"/>
+                        </a>
+                    </div>
+
+                    <div class="text-block white">
+                        <div class="content">
+                            <blockquote>
+                                <img class="source-image" src="images/jessica.jpg"/>
+                                <p>Phabulous Photo Phanatics gave me the portfolio I could only have dreamed of. Their industry knowledge was invaluable, and helped me secure a modelling job based on my professional quality portfolio shots.</p>
+                                <cite>Jessica Jones, Surrey</cite>
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <div class="image-block one">
+                        <a class="fancybox" href="/images/model-l.jpg">
+                            <img src="/images/model-s.jpg" srcset="/images/model-m.jpg 640w, /images/model-l.jpg 1200w" alt="Fashion Model Posing"/>
+                        </a>
+                    </div>
+
+                    <div class="text-block white">
+                        <div class="content">
+                            <p>From catwalks to modelling shoots, our services are wide and diverse. Employ our photography skills to satisfy your fashion image requirements.</p>
+                            <p>Your images will be made available in the cloud, on digital media, and as a stunning hard copy portfolio album. Use the best. Use Phabulous Photo Phanatics.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php include("footer.php"); ?>
+		</div>
+	</div>
+
+    <?php include("back-to-top.php"); ?>
+    <?php include("javascript-files.php"); ?>
+
+</body>
