191567530c4c17843d0aa81b0d93c6cd1f79d897
Luke Twomey <luke.twomey85@gmail.com>
2016-05-24 22:03:28 +0100

    Create basic portrait page.

---
+++ portrait.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+
+<head>
+	<?php include("head.php"); ?>
+</head>
+
+<body>
+	<div id="fouc">
+		<?php include("nav.php"); ?>
+
+        <div class="site-wrap">
+			<?php include("header.php"); ?>
+
+            <div class="page-content portrait">
+                <img class="banner-image" src="/images/weddings-s.jpg" srcset="/images/weddings-m.jpg 640w, /images/weddings-l.jpg 1200w" alt="Close crop of a camera"/>
+                <div class="rest-of-page">
+
+                    <div class="text-block white">
+                        <div class="content">
+                            <h1>Weddings</h1>
+                            <h3>Let us take away the worry of the big day. Put your faith in us, and we promise to provide you with amazing memories</h3>
+                            <p>When it comes to weddings, we know what we're doing. Let us take care of the hard work, and be safe in the knowledge that the experts have it under control.</p>
+                        </div>
+                    </div>
+
+                    <div class="image-block four">
+                        <a class="fancybox" href="/images/wedding-1-l.jpg">
+                            <img src="/images/wedding-1-s.jpg" srcset="/images/wedding-1-m.jpg 640w, /images/wedding-1-l.jpg 1200w" alt="Wedding decoration outside"/>
+                        </a>
+                        <a class="fancybox" href="/images/wedding-2-l.jpg">
+                            <img src="/images/wedding-2-s.jpg" srcset="/images/wedding-2-m.jpg 640w, /images/wedding-2-l.jpg 1200w" alt="Wedding couple hugging"/>
+                        </a>
+                        <a class="fancybox" href="/images/wedding-5-l.jpg">
+                            <img src="/images/wedding-5-s.jpg" srcset="/images/wedding-5-m.jpg 640w, /images/wedding-5-l.jpg 1200w" alt="Bride from behind"/>
+                        </a>
+                        <a class="fancybox" href="/images/wedding-3-l.jpg">
+                            <img src="/images/wedding-3-s.jpg" srcset="/images/wedding-3-m.jpg 640w, /images/wedding-3-l.jpg 1200w" alt="Wedding car"/>
+                        </a>
+                    </div>
+
+                    <div class="text-block white">
+                        <div class="content">
+                            <blockquote>
+                                <img class="source-image" src="images/charlotte.jpg"/>
+                                <p>I can't tell you how much of a relief it was to know the wedding photos were in safe hands. It was such a weight off my shoulders and allowed me to worry about the other two hundred things I needed to remember!</p>
+                                <cite>Charlotte Baker, Bristol</cite>
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <div class="image-block one">
+                        <a class="fancybox" href="/images/rings-flower-l.jpg">
+                            <img src="/images/rings-flower-s.jpg" srcset="/images/rings-flower-m.jpg 640w, /images/rings-flower-l.jpg 1200w" alt="Wedding rings next to a flower"/>
+                        </a>
+                    </div>
+
+                    <div class="text-block white">
+                        <div class="content">
+                            <p>We offer services to suit every need. We can cover the ceremony, reception and any other events you require.</p>
+                            <p>All our photographers regularly upload photos taken to the cloud, so you can be reassured there will be no danger of losing any photos of your big day! We can provide digital copies as well as beautiful printed albums for you to cherish for years to come.</p>
+                            <a href="contact.php" class="button">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php include("footer.php"); ?>
+		</div>
+	</div>
+
+    <?php include("back-to-top.php"); ?>
+    <?php include("javascript-files.php"); ?>
+
+</body>
