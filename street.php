5e9bb1f447ee3666222985b4f1f9b2a2855dd908
Luke Twomey <luke.twomey85@gmail.com>
2016-05-25 20:45:20 +0100

    Create street page.

---
+++ street.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+
+<head>
+	<?php include("head.php"); ?>
+</head>
+
+<body>
+	<div id="fouc">
+		<?php include("nav.php"); ?>
+
+        <div class="site-wrap">
+			<?php include("header.php"); ?>
+
+            <div class="page-content portrait">
+                <img class="banner-image" src="/images/portrait-banner-s.jpg" srcset="/images/portrait-banner-m.jpg 640w, /images/portrait-banner-l.jpg 1200w" alt="Close crop of a camera"/>
+                <div class="rest-of-page">
+
+                    <div class="text-block white">
+                        <div class="content">
+                            <h1>Street</h1>
+                            <h3>We can help with professional quality portrait images for a multitude of uses</h3>
+                            <p>Whether it's for a contact page for your brand new business website, or a birthday present for your mum, Phabulous Photo Phanatics has you covered.</p>
+                        </div>
+                    </div>
+
+                    <div class="image-block four">
+                        <a class="fancybox" href="/images/portrait-1-l.jpg">
+                            <img src="/images/portrait-1-s.jpg" srcset="/images/portrait-1-m.jpg 640w, /images/portrait-1-l.jpg 1200w" alt="Portrait woman"/>
+                        </a>
+                        <a class="fancybox" href="/images/portrait-2-l.jpg">
+                            <img src="/images/portrait-2-s.jpg" srcset="/images/portrait-2-m.jpg 640w, /images/portrait-2-l.jpg 1200w" alt="Portrait man"/>
+                        </a>
+                        <a class="fancybox" href="/images/portrait-5-l.jpg">
+                            <img src="/images/portrait-5-s.jpg" srcset="/images/portrait-5-m.jpg 640w, /images/portrait-5-l.jpg 1200w" alt="Portrait woman"/>
+                        </a>
+                        <a class="fancybox" href="/images/portrait-4-l.jpg">
+                            <img src="/images/portrait-4-s.jpg" srcset="/images/portrait-4-m.jpg 640w, /images/portrait-4-l.jpg 1200w" alt="Portrait woman"/>
+                        </a>
+                    </div>
+
+                    <div class="text-block white">
+                        <div class="content">
+                            <blockquote>
+                                <img class="source-image" src="images/martin.jpg"/>
+                                <p>I needed professional portrait photos taken for my new business website. You guys made it quick and easy, and I'm delighted with the results. Thanks so much!</p>
+                                <cite>Martin Canter, Portsmouth</cite>
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <div class="image-block one">
+                        <a class="fancybox" href="/images/portrait-3-l.jpg">
+                            <img src="/images/portrait-3-s.jpg" srcset="/images/portrait-3-m.jpg 640w, /images/portrait-3-l.jpg 1200w" alt="Portrait woman"/>
+                        </a>
+                    </div>
+
+                    <div class="text-block white">
+                        <div class="content">
+                            <p>We'll travel to you, no need to come to one of our studios. You can opt for a casual type of shoot, or more formal for a business environment.</p>
+                            <p>We're the number one option if you need exceptional quality portrait photos.</p>
+                            <a href="contact.php" class="button">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php include("footer.php"); ?>
+		</div>
+	</div>
+
+    <?php include("back-to-top.php"); ?>
+    <?php include("javascript-files.php"); ?>
+
+</body>
