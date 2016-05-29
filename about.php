255cc7c9a948cac7bf5f2d318d7ac6247f32f560
Luke Twomey <luke.twomey85@gmail.com>
2016-05-29 15:32:15 +0100

    Add text to about page.

--- about.php
+++ about.php
@@ -34,48 +34,10 @@
                         </div>
                     </div>
 
-                    <div class="service">
-                        <div class="image-block">
-                            <a href="fashion.php">
-                                <img src="/images/services-fashion-s.jpg" srcset="/images/services-fashion-m.jpg 640w, /images/services-fashion-l.jpg 1200w" alt="Fashion Model Posing"/>
-                            </a>
-                        </div>
-                        <div class="text-block white">
-                            <div class="content">
-                                <a href="fashion.php"><h2>Fashion</h2></a>
-                                <p>Your portfolio is in great hands. We will scout the perfect location and make you look amazing. Whether for personal use, or to secure future modelling jobs, our work will ensure you stand out from the crowd.</p>
-                                <a href="fashion.php" class="button">Find Out More</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="service">
-                        <div class="image-block">
-                            <a href="weddings.php">
-                                <img src="/images/services-wedding-s.jpg" srcset="/images/services-wedding-m.jpg 640w, /images/services-wedding-l.jpg 1200w" alt="Married Couple"/>
-                            </a>
-                        </div>
-                        <div class="text-block white">
-                            <div class="content">
-                                <a href="weddings.php"><h2>Weddings</h2></a>
-                                <p>We know how important your special day is. Leave everything to us and relax, safe in the knolwedge that the experts have it all under control.</p>
-                                <a href="weddings.php" class="button">Find Out More</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="service">
-                        <div class="image-block">
-                            <a href="portrait.php">
-                                <img src="/images/services-portrait-s.jpg" srcset="/images/services-portrait-m.jpg 640w, /images/services-portrait-l.jpg 1200w" alt="Man Smoking"/>
-                            </a>
-                        </div>
-                        <div class="text-block white">
-                            <div class="content">
-                                <a href="portrait.php"><h2>Portrait</h2></a>
-                                <p>It's all about you. Conventional or unusual, you decide. Whatever look you decide to go with, we'll make sure you look your best and get the perfect shot you're looking for.</p>
-                                <a href="portrait.php" class="button">Find Out More</a>
-                            </div>
+                    <div class="text-block white">
+                        <div class="content">
+                            <p>Phabulous Photo Phanatics was established in 1990, by Peter Wirecroft. Then a lone photographer, he has since built the company into what it is today, consisting of over two hundred professional photographers country-wide.</p>
+                            <p>Initially working solely as a wedding photographer, Peter joined forces with renowned portrait expert John Sitstill in 1994, and together they rapidly grew the business, without losing sight of the quality of work that the company is known for.</p>
                         </div>
                     </div>
 
