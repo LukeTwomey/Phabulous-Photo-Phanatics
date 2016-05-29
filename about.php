ede66beb0c49bbe5cf8095344e9dc01532a406dc
Luke Twomey <luke.twomey85@gmail.com>
2016-05-29 14:13:20 +0100

    Create about page.

---
+++ about.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+
+<head>
+	<?php include("head.php"); ?>
+</head>
+
+<body>
+	<div id="fouc">
+		<?php include("nav.php"); ?>
+
+        <div class="site-wrap">
+			<?php include("header.php"); ?>
+
+            <div class="page-content about">
+                <img class="banner-image" src="/images/services-s.jpg" srcset="/images/services-m.jpg 640w, /images/services-l.jpg 1200w" alt="Close crop of a camera"/>
+                <div class="rest-of-page">
+
+                    <div class="text-block white">
+                        <div class="content">
+                            <h1>About</h1>
+                            <h3>Phabulous Photo Phanatics provide our customers with professional, quality photography services</h3>
+                            <p>Our expert photographers will consult with you closely, to ensure the work undertaken meets your exacting standards.</p>
+                        </div>
+                    </div>
+
+                    <div class="service">
+                        <div class="image-block">
+                            <a href="fashion.php">
+                                <img src="/images/services-fashion-s.jpg" srcset="/images/services-fashion-m.jpg 640w, /images/services-fashion-l.jpg 1200w" alt="Fashion Model Posing"/>
+                            </a>
+                        </div>
+                        <div class="text-block white">
+                            <div class="content">
+                                <a href="fashion.php"><h2>Fashion</h2></a>
+                                <p>Your portfolio is in great hands. We will scout the perfect location and make you look amazing. Whether for personal use, or to secure future modelling jobs, our work will ensure you stand out from the crowd.</p>
+                                <a href="fashion.php" class="button">Find Out More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="service">
+                        <div class="image-block">
+                            <a href="weddings.php">
+                                <img src="/images/services-wedding-s.jpg" srcset="/images/services-wedding-m.jpg 640w, /images/services-wedding-l.jpg 1200w" alt="Married Couple"/>
+                            </a>
+                        </div>
+                        <div class="text-block white">
+                            <div class="content">
+                                <a href="weddings.php"><h2>Weddings</h2></a>
+                                <p>We know how important your special day is. Leave everything to us and relax, safe in the knolwedge that the experts have it all under control.</p>
+                                <a href="weddings.php" class="button">Find Out More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="service">
+                        <div class="image-block">
+                            <a href="portrait.php">
+                                <img src="/images/services-portrait-s.jpg" srcset="/images/services-portrait-m.jpg 640w, /images/services-portrait-l.jpg 1200w" alt="Man Smoking"/>
+                            </a>
+                        </div>
+                        <div class="text-block white">
+                            <div class="content">
+                                <a href="portrait.php"><h2>Portrait</h2></a>
+                                <p>It's all about you. Conventional or unusual, you decide. Whatever look you decide to go with, we'll make sure you look your best and get the perfect shot you're looking for.</p>
+                                <a href="portrait.php" class="button">Find Out More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="service">
+                        <div class="image-block">
+                            <a href="street.php">
+                                <img src="/images/services-street-s.jpg" srcset="/images/services-street-m.jpg 640w, /images/services-street-l.jpg 1200w" alt="Businessmen in New York"/>
+                            </a>
+                        </div>
+                        <div class="text-block white">
+                            <div class="content">
+                                <a href="street.php"><h2>Street</h2></a>
+                                <p>Our street photography captures the essence of a city. Get underneath the skin of the place, and live it through the people who know it best.</p>
+                                <a href="street.php" class="button">Find Out More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <?php include("footer.php"); ?>
+		</div>
+	</div>
+
+    <?php include("back-to-top.php"); ?>
+    <?php include("javascript-files.php"); ?>
+
+</body>
