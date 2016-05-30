46cd4e3a0dd7943e409cfef8a21e721362392b56
Luke Twomey <luke.twomey85@gmail.com>
2016-05-30 16:59:07 +0100

    Add contact form.

--- contact.php
+++ contact.php
@@ -18,56 +18,20 @@
                     <div class="text-block white">
                         <div class="content">
                             <h1>Contact</h1>
-                            <h3>We're here. Hit us up!</h3>
+                            <h3>If you'd like to discuss a job or simply have an informal chat, we're a shutter click away.</h3>
                         </div>
                     </div>
 
-                    <div class="image-text-block">
-                        <div class="content">
-                            <div class="image-part">
-                                <img src="/images/equipment-m.jpg"/>
-                            </div>
-                            <div class="text-part">
-                                <h3>Our state of the art equipment means our results are unbeatable, and our post-processing techniques enhance the quality of our images without making them unrecognisable.</h3>
-                            </div>
+                    <form class="contact-form" method="post" name="contactForm">
+                        <div class="container">
+                            <input id="name" name="name" required placeholder="Name" type="text"/>
+                            <input id="email" name="email" required placeholder="E-mail" type="email"/>
+                            <input name="subject" value=" has sent a new message" type="hidden"/>
+                            <textarea id="message" name="message" required rows="3" placeholder="Message"></textarea>
+                            <input type="submit" value="Send"/>
                         </div>
-                    </div>
+                    </form>
 
-                    <div class="text-block white">
-                        <div class="content">
-                            <p>Phabulous Photo Phanatics was established in 1990, by Peter Wirecroft. Then a lone photographer, he has since built the company into what it is today, consisting of over two hundred professional photographers country-wide.</p>
-                            <p>Initially working solely as a wedding photographer, Peter joined forces with renowned portrait expert John Sitstill in 1994, and together they rapidly grew the business, without losing sight of the quality of work that the company is known for.</p>
-                        </div>
-                    </div>
-
-                    <div class="image-block three">
-                        <a class="fancybox" href="/images/lens-1-l.jpg">
-                            <img src="/images/lens-1-s.jpg" srcset="/images/lens-1-m.jpg 640w, /images/lens-1-l.jpg 1200w" alt="Close up of camera lens"/>
-                        </a>
-                        <a class="fancybox" href="/images/lens-2-l.jpg">
-                            <img src="/images/lens-2-s.jpg" srcset="/images/lens-2-m.jpg 640w, /images/lens-2-l.jpg 1200w" alt="Close up of camera lens"/>
-                        </a>
-                        <a class="fancybox" href="/images/lens-3-l.jpg">
-                            <img src="/images/lens-3-s.jpg" srcset="/images/lens-3-m.jpg 640w, /images/lens-3-l.jpg 1200w" alt="Close up of camera lens"/>
-                        </a>
-                    </div>
-
-                    <div class="text-block white">
-                        <div class="content">
-                            <p>Over the years our focus has expanded to cover multiple genres, but our overriding philosophy remains the same. We love what we do, and we show that in our work.</p>
-                            <p>Let us handle your photography requirements, and you can rest assured we'll deliver.</p>
-                            <a href="contact.php" class="button">Contact Us</a>
-                        </div>
-                    </div>
-
-                    <div class="tile">
-                        <img src="/images/reflection-s.jpg" srcset="/images/reflection-m.jpg 640w, /images/reflection-l.jpg 1200w" alt="Reflection of photographer taking photo"/>
-                        <h2>Photography is
-                            <br/>a lifestyle.
-                            <br/>We live and
-                            <br/>breathe it.
-                        </h2>
-                    </div>
                 </div>
             </div>
 
