350341b0e1f3ba6deebd5f0633de299d5f42a1e0
Luke Twomey <luke.twomey85@gmail.com>
2016-05-30 18:03:53 +0100

    Tidy javascript-files.php.

--- javascript-files.php
+++ javascript-files.php
@@ -3,6 +3,4 @@
 <script type="text/javascript" src="/javascript/jquery.fancybox.pack.js"></script>
 <script src="https://npmcdn.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
 <script src="https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js"></script>
-<!--    <script src="/javascript/jquery.flexslider.js"></script>-->
 <script src="/javascript/javascript.js" type="text/javascript"></script>
-
