f3e03dad4c580fee6cd7dc2c66f4f2bd639d77a8
Luke Twomey <luke.twomey85@gmail.com>
2016-06-05 16:42:45 +0100

    Package files for deployment to web.

--- javascript-files.php
+++ javascript-files.php
@@ -1,6 +1,6 @@
-<script type="application/javascript" src="/javascript/fastclick.js"></script>
+<script type="application/javascript" src="/javascript/fastclick-min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script type="text/javascript" src="/javascript/jquery.fancybox.pack.js"></script>
 <script src="https://npmcdn.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
 <script src="https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js"></script>
-<script src="/javascript/javascript.js" type="text/javascript"></script>
+<script src="/javascript/javascript-min.js" type="text/javascript"></script>
