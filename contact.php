1d121fb4d758815784ac6de06a7dff3c28f8c74a
Luke Twomey <luke.twomey85@gmail.com>
2016-06-05 15:10:25 +0100

    Add map to contact page.

--- contact.php
+++ contact.php
@@ -22,8 +22,33 @@
                         </div>
                     </div>
 
-                     <div id="map">
-<!--                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d79495.3250121344!2d-0.16916022852688156!3d51.49066673401708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon!5e0!3m2!1sen!2suk!4v1464624642105" width="2000" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>-->
+                    <div class="contact-details">
+                        <ul>
+                            <li>
+                                <ul>
+                                    <li>Twitter</li>
+                                    <li>Facebook</li>
+                                    <li>Instagram</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li>Twitter</li>
+                                    <li>Facebook</li>
+                                    <li>Instagram</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li>Twitter</li>
+                                    <li>Facebook</li>
+                                    <li>Instagram</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div id="map">
                     </div>
 
                     <form class="contact-form" method="post" name="contactForm">
